fb3ae8d790d1a4013386a67a46ace8a99a61feb5
CallumVass <callum.vass@gmail.com>
2013-05-07 20:36:33 +0100

    Tidied code

--- DataAccess/UserRepository.cs
+++ DataAccess/UserRepository.cs
@@ -12,22 +12,12 @@ namespace DataAccess
 {
     public sealed class UserRepository : Repository<User>, IUserRepository
     {
-        private readonly IRoleRepository _roleRepo;
-
-        public UserRepository(IRoleRepository roleRepo)
-        {
-            _roleRepo = roleRepo;
-        }
-
         public void ChangeUserRole(int userId, int roleId)
         {
-            using (IDbConnection cn = Connection)
-            {
-                cn.Open();
-                var user = GetById(userId);
-                user.RoleId = roleId;
-                Update(user);
-            }
+            var user = GetById(userId);
+            user.RoleId = roleId;
+            Update(user);
+
         }
     }
 }
